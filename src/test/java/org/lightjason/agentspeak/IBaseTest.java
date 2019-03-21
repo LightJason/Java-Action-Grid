6ea2ba73ebb9102335314b8851bc88686d01f95b
Philipp Kraus <philipp.kraus@flashpixx.de>
2019-03-21 15:18:53 +0100

    fix structure

--- src/test/java/org/lightjason/agentspeak/IBaseTest.java
+++ src/test/java/org/lightjason/agentspeak/IBaseTest.java
@@ -52,12 +52,7 @@ import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
